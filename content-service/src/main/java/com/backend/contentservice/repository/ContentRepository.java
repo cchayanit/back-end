3db097ddf17fd705a06df270e9eb485f527b778c
cchayanit <chayanit.pm@gmail.com>
2021-04-29 10:04:10 +0700

    add repository and app.prop

--- content-service/src/main/java/com/backend/contentservice/repository/ContentRepository.java
+++ content-service/src/main/java/com/backend/contentservice/repository/ContentRepository.java
@@ -1,7 +1,10 @@
 package com.backend.contentservice.repository;
 
+import com.backend.contentservice.model.Content;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface ContentRepository  {
-    
+public interface ContentRepository extends JpaRepository<Content, Long>{
+      List<Content> findByTitleContaining(String title);
 }
