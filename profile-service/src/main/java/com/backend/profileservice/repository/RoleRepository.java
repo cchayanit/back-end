10db766fb775360efe7a8b94d0db313ecd4dc148
cchayanit <chayanit.pm@gmail.com>
2021-05-06 15:25:18 +0700

    add repository and other file

--- profile-service/src/main/java/com/backend/profileservice/repository/RoleRepository.java
+++ profile-service/src/main/java/com/backend/profileservice/repository/RoleRepository.java
@@ -1,14 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.backend.profileservice.repository;
 
-/**
- *
- * @author user
- */
-public interface RoleRepository {
+import com.backend.profileservice.models.ERole;
+import com.backend.profileservice.models.Role;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(ERole name);
     
 }
--- profile-service/src/main/java/com/backend/profileservice/repository/UserRepository.java
+++ profile-service/src/main/java/com/backend/profileservice/repository/UserRepository.java
@@ -1,14 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.backend.profileservice.repository;
 
-/**
- *
- * @author user
- */
-public interface UserRepository {
+import com.backend.profileservice.models.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+    Optional<User> findByUsername(String username);
+    
+    Boolean existsByUsername(String username);
+    
+    Boolean existsByEmail(String email);
+    
     
 }
