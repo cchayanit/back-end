68040fe19a56d1d94741eb4c023e5cea1200917d
cchayanit <chayanit.pm@gmail.com>
2021-05-06 15:24:20 +0700

    add model,request,response

---
+++ profile-service/src/main/java/com/backend/profileservice/models/ERole.java
@@ -0,0 +1,7 @@
+package com.backend.profileservice.models;
+
+public enum ERole{
+    ROLE_USER,
+    ROLE_MODERATOR,
+    ROLE_ADMIN
+}
--- profile-service/src/main/java/com/backend/profileservice/models/Role.java
+++ profile-service/src/main/java/com/backend/profileservice/models/Role.java
@@ -1,14 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.backend.profileservice.models;
 
-/**
- *
- * @author user
- */
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
     
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERole name;
+    
+    public Role(){
+    }
+    
+    public Role(ERole name){
+        this.name = name;
+    }
+    
+    public Integer getID(){
+        return id;
+    }
+    
+    public void setId(Integer id){
+        this.id = id;
+    }
+    
+    public ERole getName(){
+        return name;
+    }
+    
+    public void setName(ERole name){
+        this.name = name;
+    }
 }
+
+
